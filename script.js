abda29b6ff3e5e9906fe712937150f20f3503c85
pipaaa <86318578+pipaaa@users.noreply.github.com>
2023-09-24 12:06:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ document.addEventListener("DOMContentLoaded", function () {
 function navigateTo(page) {
     window.location.href = page;
 }
+// JavaScript para ocultar el aviso de cookies al hacer clic en "Aceptar"
+document.addEventListener("DOMContentLoaded", function () {
+    const cookieBanner = document.getElementById("cookie-banner");
+    const acceptCookiesButton = document.getElementById("accept-cookies");
+
+    acceptCookiesButton.addEventListener("click", function () {
+        cookieBanner.classList.remove("show-cookie-banner"); // Ocultamos el aviso de cookies
+    });
+
+    // Mostramos el aviso de cookies con la animación de deslizamiento
+    setTimeout(function () {
+        cookieBanner.classList.add("show-cookie-banner");
+    }, 500); // Mostrar después de 500 ms (medio segundo)
+});
