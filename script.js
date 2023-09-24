a46c6cec72c0c42ec82e91a2ea3d44b7c43ba8f0
pipaaa <86318578+pipaaa@users.noreply.github.com>
2023-09-24 11:44:28 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript para la animación del logo
+document.addEventListener("DOMContentLoaded", function () {
+    const logo = document.getElementById("logo");
+    logo.style.animation = "fadeIn 2s ease-in-out";
+});
