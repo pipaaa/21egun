49e697f4ee4e18827d5450ed327226c584873377
pipaaa <86318578+pipaaa@users.noreply.github.com>
2023-09-24 12:10:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const acceptCookiesButton = document.getElementById("accept-cookies");
 
     acceptCookiesButton.addEventListener("click", function () {
-        cookieBanner.style.display = "none";
-    });
-});
-
-// Función para redireccionar a las subpáginas
-function navigateTo(page) {
-    window.location.href = page;
-}
-// JavaScript para ocultar el aviso de cookies al hacer clic en "Aceptar"
-document.addEventListener("DOMContentLoaded", function () {
-    const cookieBanner = document.getElementById("cookie-banner");
-    const acceptCookiesButton = document.getElementById("accept-cookies");
-
-    acceptCookiesButton.addEventListener("click", function () {
-        cookieBanner.classList.remove("show-cookie-banner"); // Ocultamos el aviso de cookies
+        cookieBanner.classList.remove("show-cookie-banner");
     });
 
-    // Mostramos el aviso de cookies con la animación de deslizamiento
     setTimeout(function () {
         cookieBanner.classList.add("show-cookie-banner");
-    }, 500); // Mostrar después de 500 ms (medio segundo)
+    }, 500);
+
+    // Función para redireccionar a las subpáginas
+    function navigateTo(page) {
+        window.location.href = page;
+    }
 });
