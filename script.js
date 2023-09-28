b1d96df4eed28f4789f58fe8dabda0a82f454ccf
pipaaa <86318578+pipaaa@users.noreply.github.com>
2023-09-28 12:24:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-// JavaScript para mostrar el aviso de cookies
-document.addEventListener("DOMContentLoaded", function () {
-    const cookieBanner = document.getElementById("cookie-banner");
-    const acceptCookiesButton = document.getElementById("accept-cookies");
+// Código JavaScript para manejar comentarios y foro
+// (Debes implementar la lógica para cargar y publicar comentarios y hilos)
 
-    acceptCookiesButton.addEventListener("click", function () {
-        cookieBanner.style.display = "none";
-    });
+// Código para cargar comentarios en la sección de comunicados
+// Ejemplo: Agregar comentarios a la lista con id "comentarios1"
+// ...
 
-    // Mostrar el aviso de cookies
-    cookieBanner.style.display = "block";
+// Código para cargar hilos en la sección de foro
+// Ejemplo: Agregar hilos a la lista con id "hilos"
+// ...
+
+// Código para publicar comentarios en la sección de comunicados
+document.getElementById("comentario-form1").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Obtener nombre y comentario desde el formulario
+    var nombre = document.getElementById("nombre1").value;
+    var comentario = document.getElementById("comentario1").value;
+    // Debes implementar la lógica para publicar el comentario
+    // y luego agregarlo a la lista de comentarios
+    // Ejemplo: 
+    // var comentarioItem = document.createElement("li");
+    // comentarioItem.innerHTML = `<strong>${nombre}</strong>: ${comentario}`;
+    // document.getElementById("comentarios1").appendChild(comentarioItem);
+    // Limpiar el formulario
+    document.getElementById("nombre1").value = "";
+    document.getElementById("comentario1").value = "";
+});
+
+// Código para crear hilos en la sección de foro
+document.getElementById("hilo-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Obtener título y contenido del hilo desde el formulario
+    var titulo = document.getElementById("titulo-hilo").value;
+    var contenido = document.getElementById("contenido-hilo").value;
+    // Debes implementar la lógica para crear el hilo
+    // y luego agregarlo a la lista de hilos
+    // Ejemplo:
+    // var hiloItem = document.createElement("li");
+    // hiloItem.innerHTML = `<h4>${titulo}</h4><p>${contenido}</p>`;
+    // document.getElementById("hilos").appendChild(hiloItem);
+    // Limpiar el formulario
+    document.getElementById("titulo-hilo").value = "";
+    document.getElementById("contenido-hilo").value = "";
 });
